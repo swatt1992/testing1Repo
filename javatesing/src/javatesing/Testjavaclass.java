81742bab2313aa713ca16508bd804ace6a44be7a
css116284 <css116284@HDC0007194.ad.csscorp.com>
2018-12-31 18:15:43 +0530

    changes doone ny 8th time

--- javatesing/src/javatesing/Testjavaclass.java
+++ javatesing/src/javatesing/Testjavaclass.java
@@ -5,7 +5,7 @@ public class Testjavaclass {
 	public static void main(String[] args) {
 		System.out.println("Swathi tresing 5rd time");
 		System.out.println("Swathi tresing 6st time");
-		System.out.println("Swathi tresing 7nd time");
+		System.out.println("Swathi tresing 8th time");
 
 	}
 
